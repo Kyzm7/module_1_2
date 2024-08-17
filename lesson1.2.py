ee7055e650348247fad2571c94382262c31c7c07
Kyzm7 <kyzm7@mail.ru>
2024-08-17 08:44:37 +0300

    Add files via upload

---
+++ lesson1.2.py
@@ -0,0 +1,5 @@
+quantity_completed_HT = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_one_task = Number_of_hours_spent/quantity_completed_HT
+print('Курс:',Course_name,',всего задач:', quantity_completed_HT,',затрачено часов:',Number_of_hours_spent,',среднее время выполнения:',Time_one_task,'часа')
